2e07f2532c36d4620ff8f8c7042521f17d561bb3
Jon Widener <jon.widener@gmail.com>
2024-08-05 19:13:52 -0500

    Add React

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,11 @@
 // Entry point for the build script in your package.json
-import "@hotwired/turbo-rails"
-import "./controllers"
+import '@hotwired/turbo-rails';
+import './controllers';
+import React from 'react';
+// import 'react-dom';
+// import 'react-router-dom';
+import { createRoot } from 'react-dom/client';
+
+const domNode = document.getElementById('app_root');
+const root = createRoot(domNode);
+root.render(<h1>Hello World</h1>);
