096443af876e96c07f8cf617b849b1f6cd3057ed
Jon Widener <jon.widener@gmail.com>
2024-08-05 20:32:25 -0500

    Add url to request model

--- db/migrate/20240806001444_create_requests.rb
+++ db/migrate/20240806001444_create_requests.rb
@@ -3,6 +3,7 @@ class CreateRequests < ActiveRecord::Migration[7.0]
     create_table :requests do |t|
       t.string :item_name
       t.decimal :cost
+      t.string :url
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -14,6 +14,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_08_06_001444) do
   create_table "requests", force: :cascade do |t|
     t.string "item_name"
     t.decimal "cost"
+    t.string "url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
